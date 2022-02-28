7e7b96f1ef9e9cacca2292a37a086f0c2a108837
L.S <L.S.1030@outlook.com>
2022-02-28 15:55:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Image Process Time Count
+
+## 简介
+
+图像处理时间统计
+具体操作：在图像的处理前后记录时间并输出。
+
+## 环境配置
+
+Windows10+clion+Ubuntu18.04(OKT507 with OpenCV 3.2)
---
+++ Src/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file main.cpp
+ * @date 2022/2/28
+ * @author L.S.
+*/
+#include <cstdio>
+
+int main()
+{
+  printf("Project Init!\n");
+  return 0;
+}
